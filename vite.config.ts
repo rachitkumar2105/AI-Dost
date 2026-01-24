3f361dd2798e340243bd62c461a63069a98e8289
Rachit Kumar Singh <147301141+R-ac-hi-t@users.noreply.github.com>
2026-01-25 00:19:24 +0530

    chore: optimize build chunk size and warnings

--- vite.config.ts
+++ vite.config.ts
@@ -13,4 +13,16 @@ export default defineConfig(({ mode }) => ({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  build: {
+    chunkSizeWarningLimit: 1600,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        }
+      }
+    }
+  },
 }));
